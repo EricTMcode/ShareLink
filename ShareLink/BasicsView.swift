92472cf3661a376c5e2006c39378404045f60fd2
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-10-05 14:47:55 +0200

    - First section with String and Url Sharing

--- ShareLink/BasicsView.swift
+++ ShareLink/BasicsView.swift
@@ -11,12 +11,35 @@ struct BasicsView: View {
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading, spacing: 10) {
-                //  Basic Link
+                ShareLink(item: "String Item")
+                ShareLink(item: URL(string: "https://activ-informatique.com")!)
+                ShareLink("Some Text", item: "String Item")
+                ShareLink(Text("\(Image(systemName: "plus.circle.fill")) - With SFImage"), item: "String Item")
+                ShareLink("Item Subject - No Preview", item: "String Item", subject: Text("Subject"))
+                ShareLink("Item Subject Message", item: "String Item", subject: Text("Subject"), message: Text("The Message"))
+                ShareLink("URL Subject Message", item: URL(string: "https://activ-informatique.com")!, subject: Text("Subject"), message: Text("The Message"))
+                ShareLink(item: "String Item",
+                          subject: Text("A Subject"),
+                          message: Text("The Message")) {
+                    HStack {
+                        Image(.label)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30)
+                        Text("Share it")
+                    }
+                }
+                ShareLink("Item Message", item: "String Item", message: Text("The Message"))
                 Spacer()
             }
             .font(.title)
             .padding()
             .navigationTitle("ShareLinksBasics")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    ShareLink("Share", item: "Some Text")
+                }
+            }
         }
     }
 }
--- ShareLink/ShareLinkApp.swift
+++ ShareLink/ShareLinkApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ShareLinkApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartTabView()
         }
     }
 }
