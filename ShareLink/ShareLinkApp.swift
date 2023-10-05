19e679791782870b3ca4938d8365bd995b0193c1
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-10-05 14:15:32 +0200

    Initial Commit

---
+++ ShareLink/ShareLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLinkApp.swift
+//  ShareLink
+//
+//  Created by Eric on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShareLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
